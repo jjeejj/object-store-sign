6ab953d3429b207fa95d8ad1357f0b9dd623226b
jjeejj <jwj@wenjunjiang.com>
2021-08-03 17:11:23 +0800

    chore: 升级依赖

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,10 @@
-import crypto, { HexBase64Latin1Encoding } from "crypto";
+import crypto, { BinaryToTextEncoding } from "crypto";
 
 // 计算指定的数据 指定的密钥 进行 sha1 签名 并返回对应的 base64 的编码
 export function getHMacSha1Hash(
   data: string,
   secret: string,
-  encoding: HexBase64Latin1Encoding = "base64"
+  encoding: BinaryToTextEncoding = "base64"
 ) {
   return crypto.createHmac("sha1", secret).update(data).digest(encoding);
 }
@@ -12,7 +12,7 @@ export function getHMacSha1Hash(
 // 计算指定数据的 sha1 hash 值
 export function getSha1Hash(
   data: string,
-  encoding: HexBase64Latin1Encoding = "base64"
+  encoding: BinaryToTextEncoding = "base64"
 ) {
   return crypto.createHash("sha1").update(data).digest(encoding);
 }
